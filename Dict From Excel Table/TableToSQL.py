a7cac6050ebbdf84bb77f28da283cb78702376d7
theCivilCoder <yongjunjz@hotmail.com>
2022-03-26 15:32:25 -0600

    Updated the processing of individual spreadsheets used to assist with viewing how the Primary Keys and Foreign Keys are assigned for each column. Identified that program is not set up to deal with many to many relationships so that will be updated

--- Dict From Excel Table/TableToSQL.py
+++ Dict From Excel Table/TableToSQL.py
@@ -48,38 +48,38 @@ def TableToSQL(path):
 		dfPartial = dfPartial.drop_duplicates()
 		# print(f"size: {dfPartial.shape}")
 
-		#update the index to being at 1
-		dfPartial.index = np.arange(1, len(dfPartial) + 1)
 		dfPartial.index.names = ["Primary Key"]
 
 		#call function to process the table and print out excel tables representative of the table saved on SQL software
-		printTableWithForeignKeys(idxCol, dfPartial)
+		printTableWithForeignKeys(idxCol, dfPartial, dictPrimaryKeys)
 
 		# print(dfPartial.head())
 		# print()
+		idxCol += 1 
+
+	# -------------------------------------------------
+	# # THIS IS FOR TESTING SINGLE ITERATION, CAN DELETE LATER 
+
+	# #get part of the dataframe up to the current column currently under consideration
+	# idxCol = 1
+	# dfPartial = df[df.columns.to_list()[:idxCol+1]]
+	
+	# #drop the duplicate rows
+	# # print(f"size: {dfPartial.shape}")
+	# dfPartial = dfPartial.drop_duplicates()
+	# # print(f"size: {dfPartial.shape}")
+
+	# #update the index to being at 1
+	# dfPartial.index = np.arange(1, len(dfPartial) + 1)
+	# dfPartial.index.names = ["Primary Key"]
+
+	# #call function to process the table and print out excel tables representative of the table saved on SQL software
+	# printTableWithForeignKeys(idxCol, dfPartial, dictPrimaryKeys)
+
+	# # print(dfPartial.head())
+	# # print()
 
 
-		idxCol += 1
-
-	# idxCol = 0
-	# leftCol = listCols[idxCol]
-	# rightCol = listCols[idxCol+1]
-
-	# #get list of unique entries in the left column
-	# leftColUnique = list(set(df[leftCol].to_list()))
-	# leftColUnique.sort()
-
-	# # print(df[df[leftCol] == leftColUnique[0]])
-	# # df[df[leftCol] == leftColUnique[0]].to_excel("dummy.xlsx")
-
-	# addPKsToDict(dictPrimaryKeys, leftColUnique, leftCol)
-
-
-	# # #format each string so that the text is similar to a title (ie 'Sample Title Text With Capitalized First Letter')
-	# # leftColUniqueFormatted = [cell.title() for cell in leftColUnique]
-	# # print("Unique Cells in LEFT COLUMN:")
-	# # print(leftColUnique)
-	# # print("")
 
 
 
@@ -103,18 +103,44 @@ def addPKsToDict(dictPrimaryKeys, listUniqueNames, columnName):
 	#Update the dictionary with the primary keys for all the columns
 	dictPrimaryKeys[columnName] = dictOneColumn
 
-	# print(f"Column Name = {columnName}")
-	# print(dictOneColumn)
-	# print()
+	print(f"Column Name = {columnName}")
+	print(dictOneColumn)
+	print()
 
 
 
-def printTableWithForeignKeys(idxCol, dfPartial):
+def printTableWithForeignKeys(idxCol, dfPartial, dictPrimaryKeys):
 	print(f"current idxCol = {idxCol}")
+	currentColName = dfPartial.columns.to_list()[-1].replace(" ","_")
+
+	#reorganize the dfPartial so that the latest column is on the left beside the primary key
+	dfPartial = dfPartial[[dfPartial.columns.to_list()[-1]]+dfPartial.columns.to_list()[:-1]]
+
+	#sort the table by the current column under consideration
+	dfPartial.sort_values([currentColName.replace("_"," ")], inplace=True) 
+
+	#update the index to being at 1
+	dfPartial.index = np.arange(1, len(dfPartial) + 1)
+
+
+	#add in the corresponding foreign keys for each column right of the latest column 
+	listForeignKeyColumns = dfPartial.columns.to_list()[1:]
+	# print(listForeignKeyColumns)
+	for foreignKeyColumnName in listForeignKeyColumns:
+		currentCol = dfPartial[foreignKeyColumnName].to_list()
+		dictCurrentCol = dictPrimaryKeys[foreignKeyColumnName]
+
+		#define the list of foreign keys
+		listForeignKeys = [dictCurrentCol[foreignEntity] for foreignEntity in currentCol]
+
+		#Add the foreign keys column to the dataframe
+		dfPartial[f"{foreignKeyColumnName}_ForeignKey"] = listForeignKeys
+
+
 	print(dfPartial.head())
 	print()
 
-	currentColName = dfPartial.columns.to_list()[-1].replace(" ","_")
+
 
 	dfPartial.to_excel(f"Col{idxCol +1} - {currentColName}.xlsx")
 
